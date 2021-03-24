6802a7ac098a38cbc588e9fff6a426ceaa0063b6
igoreta <ivoronline@gmail.com>
2021-03-24 10:43:44 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_test_mockmvc_responsestatus/SpringbootTestMockmvcResponsestatusApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_test_mockmvc_responsestatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestMockmvcResponsestatusApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootTestMockmvcResponsestatusApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_test_mockmvc_responsestatus/SpringbootTestMockmvcResponsestatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_test_mockmvc_responsestatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestMockmvcResponsestatusApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
